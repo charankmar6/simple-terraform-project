9e457b4d3ed66ecdc3b716bab6d3c147f77dff3a
charankmar6 <charankmar6@gmail.com>
2025-10-19 14:59:44 +0530

    updated

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.10.0"
+      version = "6.17.0"
     }
   }
 }
 
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
+  region = "ap-south-1"
 }
